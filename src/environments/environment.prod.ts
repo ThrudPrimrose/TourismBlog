a603503514dcb2950cef185fe4a6196c6519f75b
primrose <budanaz.yakup@gmail.com>
2023-03-25 17:35:26 +0400

    Fix env variables for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+const _contentfulConfig = {
+  space: 'iuul3pb05b1r',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: '18SHPu2TxGe7osUbFecxtuoFZhaEzIRMIhM-8P2CCos'
+}
+
 export const environment = {
-  production: true
+  production: false,
+  contentfulConfig: _contentfulConfig,
 };
