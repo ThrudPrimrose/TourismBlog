10eb41b9d6c62951235df34a7402d06bf2ea276a
primrose <budanaz.yakup@gmail.com>
2023-06-07 15:28:24 +0200

    Update keys

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+const _contentfulConfig = {
+  space: process.env["SPACE"],
+  environment: process.env["ENVIRONMENT"], // defaults to 'master' if not set
+  accessToken: process.env["ACCESS_TOKEN"]
+}
+
+export const environment = {
+  production: false,
+  contentfulConfig: _contentfulConfig,
+};
